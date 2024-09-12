d65a9ab6e2c9bb4cc95717ed616a8758c2bb4472
ekSTazzz01 <stas_mensun2001@mail.ru>
2024-09-12 21:45:05 +1000

    Add files via upload

---
+++ Homework03.py
@@ -0,0 +1,5 @@
+completed_homeworks = 12
+use_hours = 1.5
+cours_name ='Python'
+use_time = use_hours / completed_homeworks
+print('Курс:',cours_name,',''всего задач:', completed_homeworks,',''затрачено часов:',use_hours,',''среднее время выполнения:', use_time,'.')
