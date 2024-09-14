2f7ac6f211467449b5a252fd071b481809b0b256
ekSTazzz01 <stas_mensun2001@mail.ru>
2024-09-14 21:03:41 +1000

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Назвние вашего учебного заведения:')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ""))
+print(my_string[:1])
+print(my_string[-1:])
