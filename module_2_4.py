bfd0780653910ea36c425b58c8bfa6725395fec2
ekSTazzz01 <stas_mensun2001@mail.ru>
2024-10-07 22:58:35 +1000

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,9 +1,9 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in numbers:
+for i in range(2, 16):
      is_prime = 0
-     for j in range(2, 16):
+     for j in range(1, 16):
           if i % j == 0 and i != j and j != 1:
                is_prime += 1
      if is_prime == 0:
