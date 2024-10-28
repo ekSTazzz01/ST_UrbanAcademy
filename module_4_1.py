6a61e7f633f2d776a1ae23eeb9836cd49057bfee
ekSTazzz01 <stas_mensun2001@mail.ru>
2024-10-28 22:43:41 +1000

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+
+result1 = fm(51, 17)
+result2 = fm(8, 0)
+result3 = tm(36, 6)
+result4 = tm(41, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
