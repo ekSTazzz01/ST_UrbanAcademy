902191907f2c9a5dff0cd676961634e8f5bb4fcf
ekSTazzz01 <stas_mensun2001@mail.ru>
2024-12-14 12:09:02 +1000

    Add files via upload

---
+++ main02.py
@@ -0,0 +1,20 @@
+# 1st program
+
+print((9 ** 0.5) * 5)
+
+# 2nd program
+
+print (9.99 > 9.98, 1000 != 1000.1)
+
+# 3rd program
+a = (2 * 2 + 2)
+b = (2 * (2 + 2))
+print (a)
+print (b)
+print (a == b)
+
+# 4th program
+
+c = float('123.456')
+d = c * 10
+print(int(d % 10))
